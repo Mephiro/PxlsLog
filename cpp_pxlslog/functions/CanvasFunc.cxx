c62dd1d6c62df5c9f649be90121df7fffb66a3fa
Mephiro <tristanlecocq@free.fr>
2023-10-05 00:31:51 +0200

    Setup minimal CMake configuration

---
+++ cpp_pxlslog/functions/CanvasFunc.cxx
@@ -0,0 +1,7 @@
+#include "CanvasFunc.h"
+
+namespace pxls{
+
+
+
+}
---
+++ cpp_pxlslog/pxlslog.cxx
@@ -0,0 +1,10 @@
+#include "CanvasFunc.h"
+#include <opencv2/core.hpp>
+#include <iostream>
+
+int main(){
+
+std::cout << "Hello World" << std::endl;
+
+return 0;
+}
